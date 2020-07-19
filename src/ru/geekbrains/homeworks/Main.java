c8ef6dd9ccc1a75657cebea66f14125a36d56fef
Kosenko <Kosenko.2000@yandex.ru>
2020-07-19 19:10:00 +0500

    My second homework

--- src/ru/geekbrains/homeworks/Main.java
+++ src/ru/geekbrains/homeworks/Main.java
@@ -59,7 +59,7 @@ public class Main {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i<arr.length; i++){
             if (arr[i]<6) {
-                arr[i] *=6;
+                arr[i] *=2;
             }
             System.out.print(arr[i]);
             System.out.print(" ");
